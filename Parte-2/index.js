c699d0dc0e97c391be294e6ce66e9d6e15a9ee37
Luis Caballero <luis_cn98@hotmail.com>
2020-01-29 08:46:50 -0600

    Updated PArt 2

--- Parte-2/index.js
+++ Parte-2/index.js
@@ -3,12 +3,57 @@ let bodyParser = require( 'body-parser' );
 let mongoose = require( 'mongoose' );
 let jsonParser = bodyParser.json();
 let { DATABASE_URL, PORT } = require( './config' );
+let {BookmarkList} = require('./model');
 
 let app = express();
 
 let server;
 
 /* Tu código va aquí */
+app.put('/api/boojmarks/:id', jsonParser, (req, res) => {
+	let id = req.params.id;
+	let bodyId = req.body.id;
+	let titulo = req.body.titulo;
+	let descripcion = req.body.descripcion;
+	let url = req.body.url;
+	
+	if (!bodyId){
+		res.statusMessage = "Id no proporcionado";
+		res.status(406).send();
+	}
+	else if(id != bodyId){
+		res.statusMessage = "No coinciden los ids";
+		res.status(409).send();
+	}
+	else if(titulo == "" && descripcion == "" && url == ""){
+		res.statusMessage = "Datos no proporcionados";
+		res.status(406).send();
+
+	}
+	else {
+		let updatedBookmark = {};
+		if (titulo != ""){
+			updatedBookmark.titulo = titulo;
+		}
+		if (descripcion != ""){
+			updatedBookmark.descripcion = descripcion;
+		}
+		if (url != ""){
+			updatedBookmark.url = url;
+		}
+
+		BookmarkList.updatedBookmark(bodyId, updatedBookmark)
+			.then( result => {
+				if (result.length > 0){
+					res.status(202).json(updatedBookmark)
+				}
+
+			})
+			.catch( error => {
+				throw Error (error);
+			})
+	}
+});
 
 function runServer( port, databaseUrl ){
 	return new Promise( (resolve, reject ) => {
--- Parte-2/model.js
+++ Parte-2/model.js
@@ -3,8 +3,43 @@ let uuid = require( 'uuid' );
 
 mongoose.Promise = global.Promise;
 
+let bookmarkCollection = mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    titulo: {
+        type: String,
+        required: true
+    },
+    descripcion: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+ 
+});
+
+let Bookmark = mongoose.model('bookmarks', bookmarkCollection);
+
 /* Tu código va aquí */
 
+let BookmarkList{
+    updateBookmark : function(ident, newData){
+        return Bookmark.findOneAndUpdate({id: ident}, newData)  
+            .then( result => {
+                return result;
+            })
+            .catch ( error =>{
+                throw Error (error);
+            });
+    }
+}
+
 module.exports = {
-    
+    BookmarkList
 };
